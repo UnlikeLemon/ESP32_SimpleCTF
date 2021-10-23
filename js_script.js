9f2780ab46dc74eaf942f511c23c5fecb47507c0
UnlikeLemon <83449816+UnlikeLemon@users.noreply.github.com>
2021-10-23 08:48:22 -0700

    Update js_script.js

--- js_script.js
+++ js_script.js
@@ -7,5 +7,5 @@ let send_data = await fetch("https://lemon.requestcatcher.com/", {
     'Content-Type':'application/json;charset=utf-8'
   },
   body: JSON.stringify(json)
-};
+});
                             

